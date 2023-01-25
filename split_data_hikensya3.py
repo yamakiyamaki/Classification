f7a710b4f97c7608750fb9598913c10d66cad75a
yamaki <87925136+yamakiyamaki@users.noreply.github.com>
2023-01-25 09:09:14 +0900

    0125_1

--- split_data_hikensya3.py
+++ split_data_hikensya3.py
@@ -31,9 +31,9 @@ with open(train_file) as f:
     reader = csv.reader(f) # csvの内容を行ごとに取得
     l = [row for row in reader]
     print("l", l)
-    image_path = 'DATASETS/Emotion6/Images/'
-    dirs = os.listdir(image_path)
-    folder_dir = [f for f in dirs if os.path.isdir(os.path.join(image_path, f))]
+    image_path = 'DATASETS/Emotion6/split_data/hikensha'
+    dirs = os.listdir(image_path) #Images?hikensya?の中身?。つまりanger~suprizeの6つのフォルダーをリストとして取得。# os.listdir() : ディレクトリ構造を取得
+    folder_dir = [f for f in dirs if os.path.isdir(os.path.join(image_path, f))] #hikensyaの各フォルダーの中に　　　# isdir():ファイルが存在しているとき
 
 for i in range(len(l)-1):
     image_name = l[i+1][0].replace('.jpg', '')
@@ -48,15 +48,15 @@ neutral_path = os.path.join(output_path, "neutral")
 if not os.path.exists(neutral_path):
     os.makedirs(neutral_path)
 
-for i in range(len(l)-1):
+for i in range(len(l)-1): # l[['disgust/1.jpg', '2'],[],[],...,[]]
     image_name = l[i+1][0].replace('.jpg', '')
     for j in range(len(folder_dir)):
         if (image_name.split('/')[0]) in folder_dir[j]:
-            infe_image_path = image_path + l[i+1][0]
+            infe_image_path = image_path + l[i+1][0] # -> Images/ファイルの元々のパス
             dirpath, filename = os.path.split(infe_image_path)
-            
-            if l[i+1][1] == "7":
-                if os.path.exists(os.path.join(neutral_path, filename)):
+            ## ground_truth_train.csvの5人の被験者の判断が7だったらneutralに入れる
+            if l[i+1][1] == "7": # l[i+1][1]には五人の被験者の判断の結果が入っている
+                if os.path.exists(os.path.join(neutral_path, filename)): # 存在しているかの確認
                     newname = '{}({}){}'.format(name, j+1, ext)
                     print('new:{} -> {}'.format(infe_image_path, os.path.join(neutral_path, newname)))
                     shutil.copy(infe_image_path, os.path.join(neutral_path, newname))
@@ -70,7 +70,7 @@ for i in range(len(l)-1):
                     os.makedirs(os.path.join(output_path, folder_dir[int(l[i+1][1])]))
                 go = duplicate_rename(infe_image_path)
                 """
-                x = int(l[i+1][1])
+                x = int(l[i+1][1]) # l[i+1][1]には五人の被験者の判断の結果が入っている
                 # 移動先のファイルが既に存在する場合は、代わりの名前を見つける。
                 name, ext = os.path.splitext(filename)
                 if os.path.exists(os.path.join(output_path, folder_dir[x], filename)): ###ファイルが該当フォルダーに入らない、フォルダーの順序が違うと問題あり？
