aa8515d1027adb8f29e5eb4372ea7de046ea7c15
yamaki <87925136+yamakiyamaki@users.noreply.github.com>
2023-01-26 08:34:57 +0900

    0126_1

---
+++ split_data_h2.py
@@ -0,0 +1,47 @@
+import os
+import csv
+import shutil
+
+train_file = 'DATASETS/Emotion6/csv_emotion6/ground_truth_train.csv'
+test_file = 'DATASETS/Emotion6/csv_emotion6/ground_truth_test.csv'
+output_path = 'DATASETS/Emotion6/split_data/'
+
+with open(train_file) as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+    image_path = 'DATASETS/Emotion6/Images/'
+    dirs = os.listdir(image_path)
+    folder_dir = [f for f in dirs if os.path.isdir(os.path.join(image_path, f))]
+for i in range(len(l)-1):
+    image_name = l[i+1][0].replace('.jpg', '')
+    for j in range(len(folder_dir)):
+        if (image_name.split('/')[0]) in folder_dir[j]:
+            infe_image_path = image_path + l[i+1][0]
+            if not os.path.exists(os.path.join(output_path, 'train_images', folder_dir[j])):
+                os.makedirs(os.path.join(output_path, 'train_images', folder_dir[j]))
+            shutil.copy(infe_image_path, os.path.join(output_path, 'train_images', folder_dir[j]))
+
+with open(test_file) as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+    image_path = 'DATASETS/Emotion6/Images/'
+    dirs = os.listdir(image_path)
+    folder_dir = [f for f in dirs if os.path.isdir(os.path.join(image_path, f))]
+for i in range(len(l)-1):
+    image_name = l[i+1][0].replace('.jpg', '')
+    for j in range(len(folder_dir)):
+        if (image_name.split('/')[0]) in folder_dir[j]:
+            infe_image_path = image_path + l[i+1][0]
+            dirpath, filename = os.path.split(infe_image_path)
+            if not os.path.exists(os.path.join(output_path, 'test')):
+                os.makedirs(os.path.join(output_path, 'test'))
+            # 移動先のファイルが既に存在する場合は、代わりの名前を見つける
+            name, ext = os.path.splitext(filename)
+            if os.path.exists(os.path.join(output_path, 'test', filename)):
+                newname = '{}({}){}'.format(name, j+1, ext)
+                newpath = os.path.join(output_path, 'test')
+                print('new:{} -> {}'.format(infe_image_path, os.path.join(newpath, newname)))
+                shutil.copy(infe_image_path, os.path.join(newpath, newname))
+            else:
+                print('base:{} -> {}'.format(infe_image_path, os.path.join(output_path, 'test', filename)))
+                shutil.copy(infe_image_path, os.path.join(output_path, 'test'))
---
+++ test_h2.py
@@ -0,0 +1,46 @@
+import os
+import cv2
+import csv
+import numpy as np
+import tensorflow as tf
+
+submission_list = [['image', 'label']]
+image_size = 224
+time_number = '260153'
+
+# 同じモデルを読み込んで、重みやオプティマイザーを含むモデル全体を再作成
+#new_model = tf.keras.models.load_model('saved_model/my_model')
+new_model = tf.keras.models.load_model('./models/emotion6_{}/{}.h5'.format(time_number, time_number))
+
+test_file = "DATASETS/Emotion6/csv_emotion6/ground_truth_test.csv"
+with open(test_file) as f:
+    reader = csv.reader(f)
+    l = [row for row in reader] # 0番目は['Image', 'Label']
+    image_path = 'DATASETS/Emotion6/Images/'
+    dirs = os.listdir(image_path)
+    folder_dir = [f for f in dirs if os.path.isdir(os.path.join(image_path, f))]
+for i in range(len(l)-1):
+    image_name = l[i+1][0].replace('.jpg', '')
+
+    # 推論用画像があるディレクトリを探索
+    for j in range(len(folder_dir)):
+        if (image_name.split('/')[0]) in folder_dir[j]:
+            infe_image_path = image_path + '/' + l[i+1][0]
+            image = cv2.imread(infe_image_path)
+            image = cv2.resize(image, (image_size, image_size))
+            image = image.reshape(1, image_size, image_size, 3)
+            data = np.asarray(image) #画像データを配列へ変更
+            # print(data.shape)
+            predictions = new_model.predict(data)
+            # print(predictions)
+            results = np.argmax(predictions[0]) + 1
+            # print(results)
+            submission_list.append([l[i+1][0], results])
+
+    if(i % 50 == 0):
+        print('test_file_number:{}'.format(i))
+
+# 提出用csv書き込み
+with open('./CSV/submission_{}.csv'.format(time_number), 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(submission_list)
